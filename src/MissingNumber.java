3cc2a7efd67bfe3fb19e6d80f643a4ecbf78e11a
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-01-25 23:40:54 -0300

    something

---
+++ src/MissingNumber.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class MissingNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a  = new int[n-1];
+        long sum = 0;
+
+        for (int i = 0; i < n-1; i++) {
+            a[i] = sc.nextInt();
+            sum += a[i];
+        }
+
+        sc.close();
+
+        System.out.println((long) n * (n+1)/2 - sum);
+
+    }
+}
---
+++ src/WeirdAlgorithm.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class WeirdAlgorithm {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long n = (long) sc.nextInt();
+        System.out.printf("%d ", n);
+        while (n != 1){
+            n = n % 2 == 0 ? n / 2 : n * 3 + 1;
+            System.out.printf("%d ", n);
+        }
+    }
+}
