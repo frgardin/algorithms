7ab7d4e263fa1d46b69fd0181e9e534bcc553ece
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-01-28 12:21:57 -0300

    Repetitions code

--- src/Repetitions.java
+++ src/Repetitions.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Repetitions {
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		char[] ca = s.toCharArray();
+		int counter = 0, max = 0;
+		char acc;
+		for(int i = 0; i < ca.length; i++){
+			if (i == 0){
+				counter++;
+				continue;
+			}
+			if (counter  > max)			max = counter;
+			if (!(ca[i - 1] == ca[i])) 	counter = 0;
+			counter++;
+		}
+		System.out.println(Math.max(max, counter));
+		sc.close();
+	}    
+
 }
