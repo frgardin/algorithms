a2ddc1e66674b4e81dc869d2a7138b879a96fc89
Felipe Gardin <gardinfelipe@gmail.com>
2024-05-31 09:00:10 -0300

    final version

--- src/arrays/holczer/balazs/exercises/IntegerInversionExercise.java
+++ src/arrays/holczer/balazs/exercises/IntegerInversionExercise.java
@@ -3,35 +3,14 @@ package arrays.holczer.balazs.exercises;
 public class IntegerInversionExercise {
 
     public static int reverseInteger(int n) {
-        return getReverseInteger(n);
-    }
-
-    private static int getHighestPowerIndex(int n) {
-        int i = 1;
-        int pow = 1;
-        while (n / pow > 10) {
-            pow = getPowerOfTen(i);
-            i++;
+        int reversed = 0;
+        int remainder;
+        while (n > 0) {
+           remainder = n % 10;
+           n = n / 10;
+           reversed = reversed * 10 + remainder;
         }
-        return i - 1;
-    }
-
-    private static int getPowerOfTen(int exponent) {
-        return (int) Math.pow(10, exponent);
-    }
-
-    private static int getReverseInteger(int n) {
-        int result = 0;
-        int j = 0;
-        int i = getHighestPowerIndex(n);
-        while (i >= 0) {
-            int pow = getPowerOfTen(i);
-            result += (n / pow) * getPowerOfTen(j);
-            n -= (n / pow) * pow;
-            j++;
-            i--;
-        }
-        return result;
+        return reversed;
     }
 
     public static void main(String[] args) {
