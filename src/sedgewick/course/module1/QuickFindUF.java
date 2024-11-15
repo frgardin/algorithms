cf66d5a3b30a00516abf4fcd4a9c5f683c74d76c
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-11-15 10:54:26 -0300

    [RP-1] add Quick find class

--- src/sedgewick/course/module1/QuickFindUF.java
+++ src/sedgewick/course/module1/QuickFindUF.java
@@ -1,4 +1,42 @@
 package sedgewick.course.module1;
 
-public class QuickFindUF {
+import java.util.Arrays;
+
+public class QuickFindUF implements UF{
+
+    private final int[] id;
+
+
+    public QuickFindUF(int N) {
+        id = new int[N];
+        for(int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    @Override
+    public void union(int p, int q) {
+        int pid = id[p];
+        int qid = id[q];
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pid) {
+                id[i] = qid;
+            }
+        }
+    }
+
+    @Override
+    public boolean connected(int p, int q) {
+        return id[p] == id[q];
+    }
+
+    public static void main(String[] args) {
+        QuickFindUF quickFindUF = new QuickFindUF(5);
+
+        quickFindUF.union(1, 2);
+        quickFindUF.union(3, 4);
+        quickFindUF.union(0, 4);
+
+        System.out.println(Arrays.toString(quickFindUF.id));
+    }
 }
--- src/sedgewick/course/module1/UF.java
+++ src/sedgewick/course/module1/UF.java
@@ -1,4 +1,8 @@
 package sedgewick.course.module1;
 
 public interface UF {
+
+    void union(int p, int q);
+
+    boolean connected(int p, int q);
 }
