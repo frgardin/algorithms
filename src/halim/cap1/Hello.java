fb790c1778db5d23091ad84fb17c622b2b94073c
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-11-16 10:25:32 -0300

    [RP-3] hello world task

---
+++ src/halim/cap1/Hello.java
@@ -0,0 +1,12 @@
+package halim.cap1;
+
+public class Hello {
+	
+	public static void printHello() {
+		System.out.println("Hello World!");
+	}
+	
+	public static void main(String[] args) {
+		printHello();
+	}
+}
