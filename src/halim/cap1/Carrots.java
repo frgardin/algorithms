863ba373203f5d066a7ff210b93fec95d6317abd
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-11-17 10:11:03 -0300

    [RP-4] carrots problem

---
+++ src/halim/cap1/Carrots.java
@@ -0,0 +1,13 @@
+package halim.cap1;
+
+import java.util.Scanner;
+
+public class Carrots {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int p = sc.nextInt();
+        System.out.println(p);
+    }
+}
