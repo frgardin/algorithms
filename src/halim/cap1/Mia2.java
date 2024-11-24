f30ae52a41d1a8584b2180d637d25a053e4b0cc5
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-11-24 11:14:43 -0300

    [RP-4] change to ternary

--- src/halim/cap1/Mia2.java
+++ src/halim/cap1/Mia2.java
@@ -10,19 +10,20 @@ public class Mia2 {
     private static final String T = "Tie.\n";
 
     private static int score(int a, int b) {
-        int score;
-        if ((a == 1 && b == 2) || (a == 2 && b == 1)) score = 1000;
-        else if (a == b)                              score = 100 * a;
-        else if (a > b)                               score = a * 10 + b;
-        else                                          score = b * 10 + a;
-        return score;
+        return ((a == 1 && b == 2) || (a == 2 && b == 1)) ?
+                1000 :
+                a == b ?
+                        100 * a :
+                        a > b ?
+                                a * 10 + b :
+                                b * 10 + a;
     }
 
     private static String compare(int p1, int p2) {
         return p1 > p2 ? P1 : p2 > p1 ? P2 : T;
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(new File("src/halim/cap1/file/mia.txt"));
         while (true) {
             int s0, s1, r0, r1;
@@ -31,7 +32,7 @@ public class Mia2 {
             r0 = sc.nextInt();
             r1 = sc.nextInt();
 
-            if (s0 == 0 && s1 == 0 && r0 == 0 && r1 == 0)   break;
+            if (s0 == 0 && s1 == 0 && r0 == 0 && r1 == 0) break;
 
             System.out.print(compare(score(s0, s1), score(r0, r1)));
         }
