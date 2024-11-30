90f00ce3863d11b225c9f7c6aa0aa66b50909ddf
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-11-30 13:07:11 -0300

    [RP-4] solve artichoke problem

--- src/halim/cap1/ArticHoke.java
+++ src/halim/cap1/ArticHoke.java
@@ -1,44 +1,47 @@
 package halim.cap1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class ArticHoke {
 
-    private static double price(double p, double a, double b, double c, double d, double k) {
-        return p * (Math.sin((a * k + b)) + Math.cos(c * k + d) + 2);
+    private static double price(
+            double p,
+            double a,
+            double b,
+            double c,
+            double d,
+            double k
+    ) {
+        return p * (Math.sin(a * k + b) + Math.cos(c * k + d) + 2.0);
+    }
+
+    private static void print(double d) {
+        System.out.printf("%.7f", d);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double p, a, b, c, d, n;
+
+        double p, a, b, c, d;
+        int n;
+
         p = sc.nextDouble();
         a = sc.nextDouble();
         b = sc.nextDouble();
         c = sc.nextDouble();
         d = sc.nextDouble();
-        n = sc.nextDouble();
+        n = sc.nextInt();
 
-        double price = price(p, a, b, c, d, 1);
-        double minPrice = Double.MAX_VALUE;
-        double maxPrice = 0.0;
-        double maxDecline = 0.0;
-        double actualDecline = 0.0;
-        for (int k = 2; k <= n; k++) {
-            double newPrice = price(p, a, b, c, d, k);
-            if (newPrice < price) {
-                actualDecline = maxPrice - newPrice;
-            } else if (newPrice > price) {
-                maxDecline = Math.max(actualDecline, maxDecline);
-                actualDecline = 0;
+        double p1 = price(p, a, b, c, d, 1);
+        double de = 0.0;
+        for (int i = 2; i <= n; i++){
+            double pk = price(p, a, b, c, d, i);
+            if (pk < p1) {
+                de = Math.max(p1 - pk, de);
+            } else if (pk > p1) {
+                p1 = pk;
             }
-            if (newPrice > maxPrice) {
-                maxPrice = newPrice;
-            }
-            price = newPrice;   
         }
-        System.out.printf("%.7f", maxDecline);
+        print(de);
     }
 }
