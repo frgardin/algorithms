0bb3010db9a4e5e0a0a2328715d7197c34d5215b
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-11-30 13:41:26 -0300

    [RP-4] digits in progress

---
+++ src/halim/cap1/Digits.java
@@ -0,0 +1,28 @@
+package halim.cap1;
+
+import java.util.Scanner;
+
+public class Digits {
+
+    public static int howManyDigits(int i, int j) {
+        if (i / 10 > 0) {
+            return howManyDigits(i % 10, j + 1);
+        } else {
+            return j + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        while(!s.contains("END")) {
+            int i = Integer.parseInt(s);
+
+            System.out.printf("%d%n", howManyDigits(i, 1));
+
+            s = sc.next();
+        }
+    }
+}
