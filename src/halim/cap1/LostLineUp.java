b24dcda1036ef76e36df4b70eac1ac232841a420
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-12-02 18:35:38 -0300

    [RP-4] solve lostlineup problem

---
+++ src/halim/cap1/LostLineUp.java
@@ -0,0 +1,25 @@
+package halim.cap1;
+
+import java.util.*;
+
+public class LostLineUp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] ar = new int[n];
+        StringBuilder sb = new StringBuilder();
+        ar[0] = 1;
+        int i = 2;
+        while (n-- > 1) {
+            int a = sc.nextInt();
+            ar[a + 1] = i;
+            i++;
+        }
+
+        for (int v:ar) sb.append(v + " ");
+
+        System.out.println(sb);
+    }
+}
