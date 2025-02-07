8d6cf835ee802bdc7688f55a5d540a1ee246b09d
Felipe Gardin <gardinfelipe@gmail.com>
2025-02-07 00:12:39 -0300

    brincando

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I am back!");
+        // Testing =)
+
+        String s = "a";
+
+
+        System.out.println(Optional.of(s)
+                .filter(s1 -> s1.startsWith("a"))
+                .map(String::toUpperCase)
+                .or(() -> getString().describeConstable()));
+    }
+
+    private static String getString() {
+        System.out.println("I am passing here");
+        return "trouxa";
     }
 }
