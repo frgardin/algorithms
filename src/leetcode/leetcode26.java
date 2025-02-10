5723ebe3088518bffb0a9c19b03952a8a316156a
Felipe Gardin <gardinfelipe@gmail.com>
2025-02-09 21:26:00 -0300

    leetcode26

---
+++ src/leetcode/leetcode26.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class leetcode26 {
+
+    public static int removeDuplicates(int[] nums) {
+        int last = nums[0];
+        int c = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != last) {
+                last = nums[i];
+                nums[c] = last;
+                c++;
+            }
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2};
+
+
+        System.out.println(removeDuplicates(nums));
+        System.out.println(Arrays.toString(nums));
+    }
+}
