5906af039a9a9c6b5d1872895e57c1be9eabda19
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-02-10 19:46:50 -0300

    best solution of leetcode80

--- src/leetcode/leetcode80.java
+++ src/leetcode/leetcode80.java
@@ -17,6 +17,17 @@ public class leetcode80 {
         return c;
     }
 
+    public static int removeDuplicates2(int[] nums) {
+        int c = 2;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[c - 2]) {
+                nums[c] = nums[i];
+                c++;
+            }
+        }
+        return c;
+    }
+
     public static void main(String[] args) {
         int[] nums = {1, 1, 1, 2, 2, 3};
 
