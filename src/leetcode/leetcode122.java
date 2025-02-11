173af10681852cf0df846872817214a36e733417
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-02-11 19:01:25 -0300

    leetcode122

---
+++ src/leetcode/leetcode122.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class leetcode122 {
+
+    //GREEDY ALGORITHM
+    public static int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i] < prices[i + 1]) {
+                profit += prices[i + 1] - prices[i];
+            }
+        }
+        return profit;
+    }
+}
