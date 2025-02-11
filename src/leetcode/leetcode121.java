5d43f33006929d3f7ec75c066fa3bd8e9c5b2089
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-02-10 22:04:14 -0300

    kadane algorithm

--- src/leetcode/leetcode121.java
+++ src/leetcode/leetcode121.java
@@ -1,39 +1,23 @@
 package leetcode;
 
 public class leetcode121 {
+
+    //KADANE ALGORITHM
     public static int maxProfit(int[] prices) {
-        int maxDiff = 0;
-        int maxRight = 0;
-        int minLeft = 100_000;
-        int n = prices.length;
-        for (int i = 0; i < n / 2; i++) {
-            int right = prices[n - 1 - i];
-            int left = prices[i];
-            int currentDiff = right - left;
-            if (right > maxRight) {
-                maxRight = right;
-            }
-            if (left < minLeft) {
-                minLeft = left;
-            }
-            if (currentDiff > maxDiff) {
-                maxDiff = currentDiff;
+        int buy = prices[0];
+        int profit = 0;
+        for (int price : prices) {
+            if (buy > price) {
+                buy = price;
+            } else if (price - buy > profit) {
+                profit = price - buy;
             }
         }
-        if (n % 2 != 0) {
-            int middle = prices[n / 2];
-            if (maxDiff < (middle - minLeft)) {
-                return middle - minLeft;
-            }
-            if (maxDiff < (maxRight - middle)) {
-                return maxRight - middle;
-            }
-        }
-        return maxDiff;
+        return profit;
     }
 
     public static void main(String[] args) {
-        int[] p = {1,4,2};
+        int[] p = {1, 4, 2};
 
         System.out.println(maxProfit(p));
     }
