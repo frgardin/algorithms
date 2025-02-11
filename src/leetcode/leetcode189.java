bc81c2e3572c96e67fccea3c1a8706363f2c3b3f
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-02-10 21:12:29 -0300

    best solution for 189

--- src/leetcode/leetcode189.java
+++ src/leetcode/leetcode189.java
@@ -22,9 +22,27 @@ public class leetcode189 {
         }
     }
 
+    public void rotate2(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5,6,7};
-        int k = 3;
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 2;
 
         rotate(nums, k);
 
