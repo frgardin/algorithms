7ef24fb25466077ccddb9a817ee86f174013defb
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-02-18 22:59:04 -0300

    mais exercicios

--- src/resmat/vetores/ex1.java
+++ src/resmat/vetores/ex1.java
@@ -4,7 +4,7 @@ public class ex1 {
 
 
     public static void main(String[] args) {
-        VectorXY vetorXY = new VectorXY(-50, 10);
+        VectorXY vetorXY = new VectorXY(-50, 10, VectorXY.Method.REC);
 
         System.out.println(vetorXY.getXYAngle());
         System.out.println(vetorXY.getYXAngle());
