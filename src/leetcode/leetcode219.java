b9751952f5e94bd49e2278015c38d7e418aea29e
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-02-26 21:35:58 -0300

    some solutions for 219

--- src/leetcode/leetcode219.java
+++ src/leetcode/leetcode219.java
@@ -1,10 +1,13 @@
 package leetcode;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class leetcode219 {
 
     public static boolean containsNearbyDuplicate(int[] nums, int k) {
         int n = nums.length;
-        for (int i = 0; i < n - 1;i++) {
+        for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j - i <= k && j < n; j++) {
                 if (nums[i] == nums[j] && j - i <= k) return true;
             }
@@ -12,7 +15,17 @@ public class leetcode219 {
         return false;
     }
 
+    public static boolean containsNearbyDuplicate2(int[] nums, int k) {
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && i - nums[map.get(nums[i])] <= k) return true;
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        System.out.println(containsNearbyDuplicate(new int[]{1,2,3,1,2,3}, 1));
+        System.out.println(containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 1));
+        System.out.println(containsNearbyDuplicate2(new int[]{1, 2, 3, 1, 2, 3}, 1));
     }
 }
