fc03fb4d4845b82ac43962e163588be12bb30f4a
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-02-26 21:37:24 -0300

    some solutions for 219

--- src/leetcode/leetcode219.java
+++ src/leetcode/leetcode219.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -24,8 +25,19 @@ public class leetcode219 {
         return false;
     }
 
+    //HASHMAP is better according put function
+    public static boolean containsNearbyDuplicate3(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && i - nums[map.get(nums[i])] <= k) return true;
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         System.out.println(containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 1));
         System.out.println(containsNearbyDuplicate2(new int[]{1, 2, 3, 1, 2, 3}, 1));
+        System.out.println(containsNearbyDuplicate3(new int[]{1, 2, 3, 1, 2, 3}, 1));
     }
 }
