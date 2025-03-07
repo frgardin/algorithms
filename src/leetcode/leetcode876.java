3bae31334e9398facc04eed544d527a47cce24e8
Felipe Gardin <gardinfelipe@gmail.com>
2025-03-06 21:26:20 -0300

    solution for leetcode876

---
+++ src/leetcode/leetcode876.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import linkedlist.ListNode;
+
+public class leetcode876 {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
