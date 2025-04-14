4f1a690cc3f1de060007ec039434878600363bd1
Felipe Gardin <gardinfelipe@gmail.com>
2025-04-14 18:50:52 -0300

    start studies helio pedrini IC unicamp

---
+++ src/helio/pedrini/Recursividade.java
@@ -0,0 +1,22 @@
+package helio.pedrini;
+
+public class Recursividade {
+
+    public static int fatorial(int n) {
+        int produto = 1;
+
+        for (int i = 1; i <= n; i++)
+            produto = produto * i;
+        return produto;
+    }
+
+    public static int fatorialRecursivo(int n) {
+        if (n == 1 || n == 0) return 1;
+        return n * fatorialRecursivo(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fatorialRecursivo(10));
+        System.out.println(fatorial(10));
+    }
+}
