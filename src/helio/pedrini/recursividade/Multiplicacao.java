8edc37991c0c40782f3d7f1e2bffdb579c08eb6d
Felipe Gardin <gardinfelipe@gmail.com>
2025-04-14 19:05:15 -0300

    Multiplicacao

---
+++ src/helio/pedrini/recursividade/Multiplicacao.java
@@ -0,0 +1,18 @@
+package helio.pedrini.recursividade;
+
+public class Multiplicacao {
+
+    public static int multiplicacao(int x, int y) {
+        return x * y;
+    }
+
+    public static int multiplicacaoRecursiva(int x, int y) {
+        if (y == 1) return x;
+        return x + multiplicacaoRecursiva(x, --y);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiplicacao(10, 123));
+        System.out.println(multiplicacaoRecursiva(10, 123));
+    }
+}
