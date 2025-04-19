e869f6871ca472984701119e3efc00f7f58fc128
Felipe Gardin <gardinfelipe@gmail.com>
2025-04-19 12:31:39 -0300

    imprimir reversa

---
+++ src/helio/pedrini/recursividade/ImprimirReversa.java
@@ -0,0 +1,14 @@
+package helio.pedrini.recursividade;
+
+public class ImprimirReversa {
+
+    public static void imprimirReversa(String s) {
+        if (s.isEmpty()) return;
+        System.out.print(s.charAt(s.length() - 1));
+        imprimirReversa(s.substring(0, s.length() - 1));
+    }
+
+    public static void main(String[] args) {
+        imprimirReversa("abcd");
+    }
+}
