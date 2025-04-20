49c696988d8e27da4157efeb828ee0a9815af143
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-04-20 10:46:04 -0300

    permutacao

--- src/helio/pedrini/recursividade/Permutacao.java
+++ src/helio/pedrini/recursividade/Permutacao.java
@@ -9,24 +9,22 @@ public class Permutacao {
     }
 
     public static void permutacao(String s, int k, int n) {
+        int i;
         StringBuilder sb = new StringBuilder(s);
+
         if (k == n - 1) {
             System.out.println("sb = " + sb);
-            return;
         }
-        permutaUm(k, n, sb, k);
+        else {
+            for (i = k; i < n; i++) {
+                troca(sb, k, i);
+                permutacao(sb.toString(), k + 1, n);
+                troca(sb, k, i);
+            }
+        }
     }
-
-    private static void permutaUm(int k, int n, StringBuilder sb, int i) {
-        if (i == n) return;
-        troca(sb, k, i);
-        permutacao(sb.toString(), k + 1, n);
-        troca(sb, k, i);
-        permutaUm(k, n, sb, ++i);
-    }
-
 
     public static void main(String[] args) {
-        permutacao("abc", 0, 3);
+        permutacao("abcd", 0, 4);
     }
 }
