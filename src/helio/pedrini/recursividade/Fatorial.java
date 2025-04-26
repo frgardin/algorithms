f5feb789584a7404be8bd1cb4719a2aca36eb621
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-04-26 10:32:28 -0300

    treinando

--- src/helio/pedrini/recursividade/Fatorial.java
+++ src/helio/pedrini/recursividade/Fatorial.java
@@ -15,7 +15,7 @@ public class Fatorial {
         return n * fatorialRecursivo(n - 1);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)   {
         System.out.println(fatorialRecursivo(10));
         System.out.println(fatorial(10));
     }
