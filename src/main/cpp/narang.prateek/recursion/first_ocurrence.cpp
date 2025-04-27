f4dc70ad2c047cca7dabf61629ace4df20f3e0ea
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-04-27 18:08:52 -0300

    fix issues

--- src/main/cpp/narang.prateek/recursion/first_ocurrence.cpp
+++ src/main/cpp/narang.prateek/recursion/first_ocurrence.cpp
@@ -2,28 +2,23 @@
 
 using namespace std;
 
-auto firstOcc(int arr[], const int n, const int key) -> int {
+auto firstOcc(const int arr[], const int n, const int key) -> int {
     if (n == 0) return -1;
-    return arr[n] == key ? n : firstOcc(arr, n - 1, key);
-}
-
-auto professorSolution(int arr[], const int n, const int key) -> int {
-    if (n == 0) return -1;
-
     if (arr[0] == key) return 0;
-
-    int subIndex = professorSolution(arr + 1, n - 1, key);
-    if (subIndex != -1)  return subIndex+1;
+    int subIndex = firstOcc(arr + 1, n - 1, key);
+    if (subIndex != -1) {
+        return subIndex + 1;
+    }
     return -1;
 }
 
 auto main() -> int {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    constexpr int n = sizeof(arr)/sizeof(int);
+    int arr[] = {1, 2, 3, 4, 7, 6, 7, 8};
+    constexpr int n = sizeof(arr) / sizeof(int);
     int key;
+
 
     cin >> key;
 
     cout << firstOcc(arr, n, key) << endl;
-    cout << professorSolution(arr, n, key) << endl;
 }
