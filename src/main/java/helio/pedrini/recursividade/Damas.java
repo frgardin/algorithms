f6d6dd9c31d750767bb7a57865606645cfbd9da3
Felipe Gardin <gardinfelipe@gmail.com>
2025-04-28 20:46:07 -0300

    damas brute force half of code

---
+++ src/main/java/helio/pedrini/recursividade/Damas.java
@@ -0,0 +1,21 @@
+package helio.pedrini.recursividade;
+
+import java.util.*;
+
+public class Damas {
+
+    public static int[] damasBruteForce(int[] v, int k, int n) {
+        int[] a = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                //todo finish this method
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(damasBruteForce(new int[100], 0, 4)));
+    }
+}
