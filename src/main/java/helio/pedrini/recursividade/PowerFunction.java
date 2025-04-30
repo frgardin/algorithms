c98276985b876a7b2742d4af438ebd931f3b6fbd
Felipe Gardin <gardinfelipe@gmail.com>
2025-04-30 00:10:57 -0300

    faster power solution

--- src/main/java/helio/pedrini/recursividade/PowerFunction.java
+++ src/main/java/helio/pedrini/recursividade/PowerFunction.java
@@ -6,4 +6,19 @@ public class PowerFunction {
         if (n == 0) return 1;
         return a * power(a, n-1);
     }
+
+    private static int powerFaster(int a, int n) {
+        if (n == 0) return 1;
+
+        if ((n&1) == 1) {
+            return a * powerFaster(a, n / 2) * powerFaster(a, n / 2);
+        } else {
+            return powerFaster(a, n / 2) * powerFaster(a, n / 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(power(3, 10));
+        System.out.println(powerFaster(3, 10));
+    }
 }
