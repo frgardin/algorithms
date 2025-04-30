5b455fbd23609ef94989f1f7c79d4e9abb760245
Felipe Gardin <gardinfelipe@gmail.com>
2025-04-30 00:06:24 -0300

    first power solution

---
+++ src/main/java/helio/pedrini/recursividade/PowerFunction.java
@@ -0,0 +1,9 @@
+package helio.pedrini.recursividade;
+
+public class PowerFunction {
+
+    private static int power(int a, int n) {
+        if (n == 0) return 1;
+        return a * power(a, n-1);
+    }
+}
