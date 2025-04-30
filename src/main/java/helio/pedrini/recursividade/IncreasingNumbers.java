b6423e1b2837c0ef5d8a2bcf49519de82555f42a
Felipe Gardin <gardinfelipe@gmail.com>
2025-04-30 00:04:12 -0300

    IncreasingNumbers.java

---
+++ src/main/java/helio/pedrini/recursividade/IncreasingNumbers.java
@@ -0,0 +1,22 @@
+package helio.pedrini.recursividade;
+
+import java.util.*;
+
+public class IncreasingNumbers {
+
+    private static List<Integer> increasingNumbers(int N) {
+        List<Integer> l = new ArrayList<>();
+        if (N == 1) {
+            l.add(1);
+            return l;
+        }
+
+        List<Integer> listUpdated = increasingNumbers(N - 1);
+        listUpdated.add(N);
+        return listUpdated;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(increasingNumbers(5));
+    }
+}
