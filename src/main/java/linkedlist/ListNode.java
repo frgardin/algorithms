fe3ad3cd1183fcc7af2484a2b2d06ef9b7904597
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-05-04 09:57:02 -0300

    add listnode utils in ListNode.java

--- src/main/java/linkedlist/ListNode.java
+++ src/main/java/linkedlist/ListNode.java
@@ -30,4 +30,18 @@ public class ListNode {
         sb.append(" ]");
         return sb.toString();
     }
+
+    public static ListNode of(int... arr) {
+        ListNode head = new ListNode();
+        ListNode l = head;
+        for (int i = 0; i < arr.length; i++) {
+            l.val = arr[i];
+            if (i != arr.length - 1) {
+                l.next = new ListNode();
+                l = l.next;
+            }
+        }
+        l = null;
+        return head;
+    }
 }
