e775de396c945766d3c5a5f706b555280de5dbe9
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-05-03 22:49:04 -0300

    leetcode1128 improved

--- src/main/java/leetcode/old/leetcode1128.java
+++ src/main/java/leetcode/old/leetcode1128.java
@@ -1,7 +1,7 @@
 package leetcode.old;
 
 public class leetcode1128 {
-    class Solution {
+    class Solution1 {
         public int numEquivDominoPairs(int[][] dominoes) {
             int acc = 0;
 
@@ -15,6 +15,21 @@ public class leetcode1128 {
                 }
             }
             return acc;
+        }
+    }
+
+    class Solution2 {
+        public int numEquivDominoPairs(int[][] dominoes) {
+            int[] mpp = new int[100];
+            for (int[] d : dominoes)
+                if (d[0] > d[1]) mpp[d[0] * 10 + d[1]]++;
+                else mpp[d[1] * 10 + d[0]]++;
+
+            int count = 0;
+            for (int freq : mpp)
+                count += (freq - 1) * freq / 2;
+
+            return count;
         }
     }
 }
