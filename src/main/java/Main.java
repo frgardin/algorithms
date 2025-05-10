d9df26ee0baffb13fa8716d71e8f8e307276dda1
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-05-10 17:34:54 -0300

    testing bigdecimal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
 import java.math.BigDecimal;
-import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
         // Testing =)
-        String s = "a";
         BigDecimal bigDecimal1 = new BigDecimal("0.1");
         BigDecimal bigDecimal2 = new BigDecimal("0.7");
         System.out.println(0.1 + 0.7);
