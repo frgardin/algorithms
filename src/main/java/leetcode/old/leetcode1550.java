97a793952e61edec1db5cba3edf131cb26e8a97d
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-05-11 10:17:58 -0300

    create test for leetcode1550

--- src/main/java/leetcode/old/leetcode1550.java
+++ src/main/java/leetcode/old/leetcode1550.java
@@ -3,19 +3,17 @@ package leetcode.old;
 public class leetcode1550 {
 
     // link: https://leetcode.com/problems/three-consecutive-odds/
-    class Solution {
-        public boolean threeConsecutiveOdds(int[] arr) {
-            int c = 0;
-            int i = 0;
-            while(c != 3 && i < arr.length) {
-                if ((arr[i] & 1) == 1) {
-                    c++;
-                } else {
-                    c = 0;
-                }
-                i++;
+    public static boolean threeConsecutiveOdds(int[] arr) {
+        int c = 0;
+        int i = 0;
+        while (c != 3 && i < arr.length) {
+            if ((arr[i] & 1) == 1) {
+                c++;
+            } else {
+                c = 0;
             }
-            return c == 3;
+            i++;
         }
+        return c == 3;
     }
 }
---
+++ src/test/java/leetcode/old/leetcode1550Test.java
@@ -0,0 +1,33 @@
+package leetcode.old;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class leetcode1550Test {
+
+    @Test
+    void shouldReturnFalseForNoOddThreeSequence() {
+        //given
+        int[] arr = new int[]{2, 6, 4, 1};
+
+        //when
+        boolean result = leetcode1550.threeConsecutiveOdds(arr);
+
+        //then
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldReturnTrueForOddThreeSequence() {
+        //given
+        int[] arr = new int[]{1,2,34,3,4,5,7,23,12};
+
+        //when
+        boolean result = leetcode1550.threeConsecutiveOdds(arr);
+
+        //then
+        assertTrue(result);
+    }
+
+}
