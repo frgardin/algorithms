5057f5a0015f5fa5872c8246b5eb19649aef972a
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-05-11 10:15:11 -0300

    leetcode1550

---
+++ src/main/java/leetcode/old/leetcode1550.java
@@ -0,0 +1,21 @@
+package leetcode.old;
+
+public class leetcode1550 {
+
+    // link: https://leetcode.com/problems/three-consecutive-odds/
+    class Solution {
+        public boolean threeConsecutiveOdds(int[] arr) {
+            int c = 0;
+            int i = 0;
+            while(c != 3 && i < arr.length) {
+                if ((arr[i] & 1) == 1) {
+                    c++;
+                } else {
+                    c = 0;
+                }
+                i++;
+            }
+            return c == 3;
+        }
+    }
+}
