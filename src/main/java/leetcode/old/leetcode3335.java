13b4ebbf432acd85aa1486a3835c9e6fa573e95b
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-05-13 07:46:05 -0300

    add solution

--- src/main/java/leetcode/old/leetcode3335.java
+++ src/main/java/leetcode/old/leetcode3335.java
@@ -18,4 +18,20 @@ public class leetcode3335 {
         }
         return evaluate('a', t - 'z' + c) + evaluate('b', t - 'z' + c);
     }
+
+    class AnswerSolution {
+        private final int mod = 1_000_000_007;
+        public int lengthAfterTransformations(String s, int t) {
+            int[] dp = new int[t + 26];
+            for (int i = 0; i < 26; i++)
+                dp[i] = 1;
+            for (int i = 26; i < t + 26; i++)
+                dp[i] = (dp[i - 26] + dp[i - 25]) % mod;
+
+            int ans = 0;
+            for (char ch : s.toCharArray())
+                ans = (ans + dp[ch - 'a' + t]) % mod;
+            return ans;
+        }
+    }
 }
