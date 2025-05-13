db7045f8130fba7d68239c5bd6d1c5798564bd7e
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-05-12 22:57:15 -0300

    bug version for leetcode3335

---
+++ src/main/java/leetcode/old/leetcode3335.java
@@ -0,0 +1,25 @@
+package leetcode.old;
+
+public class leetcode3335 {
+
+    public static int lengthAfterTransformations(String s, int t) {
+        if (t == 0) {
+            return s.length();
+        }
+        return lengthAfterTransformations(transform(s), t - 1);
+    }
+
+    private static String transform(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c == 'z') {
+                sb.append("ab");
+            } else {
+                sb.append(c + 1);
+            }
+        }
+
+        return sb.toString();
+    }
+}
