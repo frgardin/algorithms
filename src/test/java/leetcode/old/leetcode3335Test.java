c15bc06f2751b6519bfc87f17e2f20d7eeea24c9
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-05-12 22:57:22 -0300

    template test

---
+++ src/test/java/leetcode/old/leetcode3335Test.java
@@ -0,0 +1,20 @@
+package leetcode.old;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class leetcode3335Test {
+
+    @Test
+    void shouldMapTotalCharactersInStringAfterTransformations() {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+}
