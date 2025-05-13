a82e6b0b858dbdef67aacc00fb61b4eda7158c5d
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-05-12 23:02:14 -0300

    initial tests

--- src/test/java/leetcode/old/leetcode3335Test.java
+++ src/test/java/leetcode/old/leetcode3335Test.java
@@ -1,20 +1,35 @@
 package leetcode.old;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class leetcode3335Test {
 
     @Test
-    void shouldMapTotalCharactersInStringAfterTransformations() {
+    void shouldMapTotalCharactersInStringAfterTransformations1() {
         //given
+        String s = "abcyy";
+        int t = 2;
 
         //when
+        int result = leetcode3335.lengthAfterTransformations(s, t);
 
         //then
+        assertEquals(7, result);
+    }
 
+    @Test
+    void shouldMapTotalCharactersInStringAfterTransformations2() {
+        //given
+        String s = "azbk";
+        int t = 1;
+
+        //when
+        int result = leetcode3335.lengthAfterTransformations(s, t);
+
+        //then
+        assertEquals(5, result);
     }
 
 }
