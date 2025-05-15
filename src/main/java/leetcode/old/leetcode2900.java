c1bd4cc0c32dfe4ed37324b9e79ffb0360a95e87
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-05-14 23:10:03 -0300

    solved

--- src/main/java/leetcode/old/leetcode2900.java
+++ src/main/java/leetcode/old/leetcode2900.java
@@ -8,52 +8,19 @@ public class leetcode2900 {
         if (words.length == 1) {
             return List.of(words);
         }
-        List<String> l0 = new LinkedList<>();
-        List<String> l1 = new LinkedList<>();
-        List<String> r = new LinkedList<>();
-        int first = groups[0];
-        for (int i = 0; i < groups.length; i++) {
-            if (groups[i] == 0) {
-                l0.add(words[i]);
-            } else {
-                l1.add(words[i]);
-            }
-        }
-        int i = 0;
-        int j = 0;
-        if (first == 0) {
-            if (l1.isEmpty()) return List.of(l0.getFirst());
+        int i = 1;
+        List<String> l = new LinkedList<>();
+        List<Integer> g = new LinkedList<>();
+        l.add(words[0]);
+        g.add(groups[0]);
 
-            while (i < l0.size() && j < l1.size()) {
-                if (i > j) {
-                    r.add(l1.get(j));
-                    j++;
-                } else {
-                    r.add(l0.get(i));
-                    i++;
-                }
+        while(i < groups.length) {
+            if (g.getLast() != groups[i]) {
+                l.add(words[i]);
+                g.add(groups[i]);
             }
-        } else {
-            if (l0.isEmpty()) return List.of(l1.getFirst());
-
-            while (i < l0.size() && j < l1.size()) {
-                if (i >= j) {
-                    r.add(l1.get(j));
-                    j++;
-                } else {
-                    r.add(l0.get(i));
-                    i++;
-                }
-            }
-        }
-        while (i < l1.size() && i < l0.size()) {
-            r.add(l0.get(i));
             i++;
         }
-        while (j < l1.size() && j < l0.size()) {
-            r.add(l1.get(j));
-            j++;
-        }
-        return r;
+        return l;
     }
 }
