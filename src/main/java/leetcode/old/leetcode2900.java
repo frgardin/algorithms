b80f296524ce9a7bd23fa09a56ff6467b9d942bc
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-05-14 23:11:18 -0300

    solved

--- src/main/java/leetcode/old/leetcode2900.java
+++ src/main/java/leetcode/old/leetcode2900.java
@@ -9,8 +9,8 @@ public class leetcode2900 {
             return List.of(words);
         }
         int i = 1;
-        List<String> l = new LinkedList<>();
-        List<Integer> g = new LinkedList<>();
+        List<String> l = new ArrayList<>();
+        List<Integer> g = new ArrayList<>();
         l.add(words[0]);
         g.add(groups[0]);
 
