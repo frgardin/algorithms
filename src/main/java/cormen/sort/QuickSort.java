d5d3e6525663cdc0a2fc624887cfccd04bf7b732
Felipe Gardin <gardinfelipe@gmail.com>
2025-05-16 22:11:24 -0300

    reset quicksort

--- src/main/java/cormen/sort/QuickSort.java
+++ src/main/java/cormen/sort/QuickSort.java
@@ -4,36 +4,8 @@ import java.util.Arrays;
 
 public class QuickSort {
 
-    private static void swap(int[] a,int p,  int r) {
-        int t = a[p];
-        a[p] = a[r];
-        a[r] = t;
-    }
-
-    private static int partition(int[] a, int p, int r) {
-        int x = a[r];
-        int i = p -1;
-        for (int j = p; j <= r - 1;j++) {
-            if (a[j] <= x) {
-                i++;
-                swap(a, i, j);
-            }
-        }
-        swap(a, i+1, r);
-        return i + 1;
-    }
-
-    public static void sort(int[] a, int p, int r) {
-        if (p < r) {
-            int q = partition(a, p, r);
-            sort(a, p, q -1);
-            sort(a, q+1, r);
-        }
-    }
-
     public static void main(String[] args) {
         int[] a = {2, 8, 7, 1, 3, 5, 6, 4};
-        sort(a, 0,a.length - 1 );
         System.out.println(Arrays.toString(a));
     }
 }
