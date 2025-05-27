34923d148fa4ce198f7c5792e76e3d1823b6a714
Felipe Gardin <gardinfelipe@gmail.com>
2025-05-27 20:15:54 -0300

    solve problem

--- src/main/java/com/algorithms/usaco/lostcow.java
+++ src/main/java/com/algorithms/usaco/lostcow.java
@@ -1,36 +1,38 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class lostcow {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new FileReader("lostcow.in"));
-		PrintWriter pw =  new PrintWriter(new PrintWriter("lostcow.out"));
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("lostcow.in"));
+        PrintWriter pw = new PrintWriter(new PrintWriter("lostcow.out"));
 
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int x = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
 
-		int acc = 0;
-		int pos = x;
-		if (x < y) {
-			for (int i = 1; pos < y; i = - 2 * i) {
-				int newPos = x + i > 0 ? x + i > y ? y : x + i: 0;
-				acc += Math.abs(newPos - pos);
-				pos = newPos;
-			}
-		} else if (x > y){
-			for (int i = 1; pos > y; i = - 2 * i) {
-				int newPos = x + i > 0 ? x + i < y ? y : x + i : 0;
-				acc += Math.abs(newPos - pos);
-				pos = newPos;
-			}
-		} 		
+        int acc = 0;
+        int pos = x;
+        if (x < y) {
+            for (int i = 1; pos < y; i *= -2) {
+                int newPos = Math.min(x + i, y);
+                acc += Math.abs(newPos - pos);
+                pos = newPos;
+            }
+        } else if (x > y) {
+            for (int i = 1; pos > y; i *= -2) {
+                int newPos = Math.max(x + i, y);
+                acc += Math.abs(newPos - pos);
+                pos = newPos;
+            }
+        }
 
-		pw.print(acc);
-		br.close();
-		pw.close();
-	}
+        pw.print(acc);
+        br.close();
+        pw.close();
+    }
 }
