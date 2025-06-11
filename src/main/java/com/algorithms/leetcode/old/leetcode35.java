be386369e65d57f17642b96ea0c0de5e6c4b8cbf
Felipe Gardin <gardinfelipe@gmail.com>
2025-06-10 23:31:45 -0300

    first version

--- src/main/java/com/algorithms/leetcode/old/leetcode35.java
+++ src/main/java/com/algorithms/leetcode/old/leetcode35.java
@@ -4,25 +4,37 @@ public class leetcode35 {
 
     public static int searchInsert(int[] nums, int target) {
         int i = nums.length / 2;
-        while (i > 0 && i < nums.length) {
+        while (i >= 0 && i < nums.length) {
             if (nums[i] < target) {
-                i /= 2;
+                if (i == nums.length - 1) {
+                    return nums.length;
+                }
+                if (nums[i + 1] > target) {
+                    return i + 1;
+                }
+                if ((nums.length -1 - i) % 2 != 0) {
+                    i = i + i / 2+1;
+                } else {
+                    i = i + i / 2;
+                }
             } else if (nums[i] > target) {
-                i = i + i / 2;
+                if (i == 0) {
+                    return 0;
+                }
+                if (nums[i-1] < target) {
+                    return i;
+                }
+                i /= 2;
             } else {
                 return i;
             }
         }
-        if (nums[0] == target) {
-            return 0;
-        }
-
-        return -1;
+        return i;
     }
 
     public static void main(String[] args) {
-        int[] ints = {1, 3, 5, 6};
-        int target = 2;
+        int[] ints = {1, 3, 5};
+        int target = 5;
 
         System.out.println(searchInsert(ints, target));
     }
