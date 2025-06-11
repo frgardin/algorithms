ac7c8e1034678667cde9ba90ae8b186a4a18aa17
Felipe Gardin <gardinfelipe@gmail.com>
2025-06-10 23:57:39 -0300

    solution for https://leetcode.com/problems/search-insert-position/

--- src/main/java/com/algorithms/leetcode/old/leetcode35.java
+++ src/main/java/com/algorithms/leetcode/old/leetcode35.java
@@ -3,38 +3,22 @@ package com.algorithms.leetcode.old;
 public class leetcode35 {
 
     public static int searchInsert(int[] nums, int target) {
-        int i = nums.length / 2;
-        while (i >= 0 && i < nums.length) {
-            if (nums[i] < target) {
-                if (i == nums.length - 1) {
-                    return nums.length;
-                }
-                if (nums[i + 1] > target) {
-                    return i + 1;
-                }
-                if ((nums.length -1 - i) % 2 != 0) {
-                    i = i + i / 2+1;
-                } else {
-                    i = i + i / 2;
-                }
-            } else if (nums[i] > target) {
-                if (i == 0) {
-                    return 0;
-                }
-                if (nums[i-1] < target) {
-                    return i;
-                }
-                i /= 2;
-            } else {
-                return i;
-            }
+        int start = 0;
+        int end = nums.length - 1;
+        int ans = nums.length;
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+            if (nums[mid] >= target) {
+                ans = mid;
+                end = mid - 1;
+            } else start = mid + 1;
         }
-        return i;
+        return ans;
     }
 
     public static void main(String[] args) {
-        int[] ints = {1, 3, 5};
-        int target = 5;
+        int[] ints = {1, 3, 5, 6};
+        int target = 7;
 
         System.out.println(searchInsert(ints, target));
     }
