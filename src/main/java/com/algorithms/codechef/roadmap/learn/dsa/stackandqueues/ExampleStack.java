399eb399dc654df084aabf5adbf03f7e55175ec4
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-06-20 10:57:04 -0300

    solve

---
+++ src/main/java/com/algorithms/codechef/roadmap/learn/dsa/stackandqueues/ExampleStack.java
@@ -0,0 +1,24 @@
+package com.algorithms.codechef.roadmap.learn.dsa.stackandqueues;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ExampleStack {
+    public static void main(String[] args) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        // Adding elements (push)
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        // Accessing the top element
+        int topElement = stack.peek();
+        System.out.println("Top element: " + topElement);
+
+        // Removing elements (pop)
+        stack.pop();
+        topElement = stack.peek();
+        System.out.println("Top element after pop: " + topElement);
+    }
+}
