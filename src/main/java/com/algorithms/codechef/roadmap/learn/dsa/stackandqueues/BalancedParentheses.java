a23ebc02092f1d6ab2d360ddc3693dac6132d85e
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-06-20 09:23:02 -0300

    solve

---
+++ src/main/java/com/algorithms/codechef/roadmap/learn/dsa/stackandqueues/BalancedParentheses.java
@@ -0,0 +1,69 @@
+package com.algorithms.codechef.roadmap.learn.dsa.stackandqueues;
+
+import java.util.Scanner;
+
+class BalancedParentheses {
+    static final int MAX_SIZE = 101;
+    static char[] a = new char[MAX_SIZE];
+    static int top = -1;
+
+    static void push(char ele) {
+        if (top <= MAX_SIZE - 1) {
+            a[++top] = ele;
+        } else {
+            System.out.println("Stack is full. Cannot push: " + ele);
+        }
+    }
+
+    static char pop() {
+        if (top >= 0) {
+            char ele = a[top];
+            top--;
+            return ele;
+        } else {
+            System.out.println("Stack is empty. Cannot pop.");
+            return '-';
+        }
+    }
+
+    static boolean isEmpty() {
+        return top == -1;
+    }
+
+    static boolean isFull() {
+        return top >= MAX_SIZE;
+    }
+
+    static boolean isMatchingPair(char open, char close) {
+        return (open == '(' && close == ')');
+    }
+
+    static boolean isBalanced(String expression) {
+        char[] a = expression.toCharArray();
+
+        int i = 0;;
+
+        while (i < a.length) {
+            if (a[i] == '(') {
+                push(a[i]);
+            } else if (isEmpty()) {
+                return false;
+            } else {
+                pop();
+            }
+            i++;
+        }
+
+        return isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            String str = scanner.next();
+            System.out.println(str + " : " + isBalanced(str));
+        }
+    }
+}
