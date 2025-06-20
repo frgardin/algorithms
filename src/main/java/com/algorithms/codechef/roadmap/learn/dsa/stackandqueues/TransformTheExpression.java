89ed018ba5ff110fe4414c4d6045d02c5d1cd3c0
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-06-20 10:57:15 -0300

    solve

---
+++ src/main/java/com/algorithms/codechef/roadmap/learn/dsa/stackandqueues/TransformTheExpression.java
@@ -0,0 +1,34 @@
+package com.algorithms.codechef.roadmap.learn.dsa.stackandqueues;
+
+import java.util.*;
+
+public class TransformTheExpression {
+
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int TC = Integer.parseInt(sc.nextLine());
+
+        while (TC-->0) {
+            StringBuilder sb = new StringBuilder();
+
+            Deque<Character> operators = new ArrayDeque<>();
+
+            String expression = sc.nextLine();
+            char[] expressionCharArray = expression.toCharArray();
+
+            for (char c : expressionCharArray) {
+                if (c == '(') ;
+                else if (c == '+') operators.push(c);
+                else if (c == '-') operators.push(c);
+                else if (c == '*') operators.push(c);
+                else if (c == '/') operators.push(c);
+                else if (c == '^') operators.push(c);
+                else if (c == ')') sb.append(operators.pop());
+                else               sb.append(c);
+            }
+            System.out.println(sb);
+        }
+    }
+}
