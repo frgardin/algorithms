e145510657b8833016e5b59f14099d9751b40b06
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-08-21 09:17:25 -0300

    dfs

--- src/main/java/com/algorithms/leetcode/graphs/bfs/leetcode200.java
+++ src/main/java/com/algorithms/leetcode/graphs/bfs/leetcode200.java
@@ -1,49 +1,29 @@
 package com.algorithms.leetcode.graphs.bfs;
 
 public class leetcode200 {
-
-    // https://leetcode.com/problems/number-of-islands/
-
-
-    private static final int[][] dir = new int[][]{{-1 ,0}, {1, 0}, {0, -1}, {0, 1}};
-    private static int col;
-    private static int row;
+    private static final int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
 
     public static int numIslands(char[][] grid) {
-        row = grid.length;
-        col = grid[0].length;
-
-        boolean[][] visited = new boolean[row][col];
-        
+        int row = grid.length;
+        int col = grid[0].length;
         int counter = 0;
 
-        for (int i =0;i<row;i++) {
-            for (int j=0;j<col;j++) {
-                if (grid[i][j] == '1' && !visited[i][j]) {
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (grid[i][j] == '1') {
                     counter++;
-                    dfs(grid, visited, new int[]{i, j});
+                    dfs(grid, i, j, row, col);
                 }
             }
         }
         return counter;
     }
 
-    private static void dfs(char[][] grid, boolean[][] visited, int[] pos) {
-        int x=pos[0], y=pos[1];
-        visited[x][y]=true;
-
+    private static void dfs(char[][] grid, int x, int y, int row, int col) {
+        if (x<0 || x>=row || y<0 || y>=col || grid[x][y] != '1') return;
+        grid[x][y] = '0'; 
         for (int[] d : dir) {
-            int i=d[0], j=d[1];
-            
-            int nx = x+i;
-            int ny = y+j;
-
-            if (nx>=0&& nx<row &&
-                ny>=0&& ny<col &&
-                grid[nx][ny] == '1' && 
-                !visited[nx][ny]
-            )
-            dfs(grid, visited, new int[]{nx, ny});
+            dfs(grid, x+d[0], y+d[1], row, col);
         }
     }
 
