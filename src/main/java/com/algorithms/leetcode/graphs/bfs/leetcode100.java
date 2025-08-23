4fa15fe63b911e07092088519d554eb71f430297
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-08-23 20:48:07 -0300

    Adds recursive solution for checking binary tree equality
    Implements a method to determine if two binary trees are structurally identical and have equal node values, supporting breadth-first search problem practice and code reuse.

---
+++ src/main/java/com/algorithms/leetcode/graphs/bfs/leetcode100.java
@@ -0,0 +1,53 @@
+package com.algorithms.leetcode.graphs.bfs;
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+// https://leetcode.com/problems/same-tree/description/?envType=problem-list-v2&envId=breadth-first-search&
+public class leetcode100 {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null) {
+            return false;
+        } else if (q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
