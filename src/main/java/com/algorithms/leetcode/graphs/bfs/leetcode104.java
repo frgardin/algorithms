aa4f99d6b0f489eb2fe36ed5b9d1dafdef8388a2
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-08-23 21:14:23 -0300

    Adds recursive solution for max binary tree depth
    Implements a recursive approach to calculate the maximum depth of a binary tree using depth-first traversal.
    Includes a simple TreeNode definition to support the algorithm.

---
+++ src/main/java/com/algorithms/leetcode/graphs/bfs/leetcode104.java
@@ -0,0 +1,36 @@
+
+package com.algorithms.leetcode.graphs.bfs;
+
+
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/?envType=problem-list-v2&envId=breadth-first-search&
+public class leetcode104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left, 1), maxDepth(root.right, 1));
+    }
+
+    private int maxDepth(TreeNode root, int depth) {
+        if (root == null) return depth;
+        return Math.max(maxDepth(root.left, depth+1), maxDepth(root.right, depth+1));
+    }
+
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
