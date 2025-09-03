9fb73fad1b347b847f1b2aa2a71fb335a634d8d2
Felipe Gardin <gardinfelipe@gmail.com>
2025-09-03 20:39:41 -0300

    naive solution

--- src/main/java/com/algorithms/codechef/roadmap/FindArrayMin.java
+++ src/main/java/com/algorithms/codechef/roadmap/FindArrayMin.java
@@ -10,82 +10,43 @@ class FindArrayMin {
 
         int t = sc.nextInt();
         while (t-- > 0) {
+            int na=sc.nextInt();
+            int nb=sc.nextInt();
+            int nc=sc.nextInt();
 
-            int na = sc.nextInt();
-            int nb = sc.nextInt();
-            int nc = sc.nextInt();
+            int aa[] = new int[na];
+            int ab[] = new int[nb];
+            int ac[] = new int[nc];
 
-            int a[] = new int[na];
-            int b[] = new int[nb];
-            int c[] = new int[nc];
-
-            for (int i = 0; i < na; i++) {
-                a[i] = sc.nextInt();
+            for (int i =0;i<na;i++) {
+                aa[i]=sc.nextInt();
             }
-            for (int i = 0; i < nb; i++) {
-                b[i] = sc.nextInt();
+            for (int i =0;i<nb;i++) {
+                ab[i]=sc.nextInt();
             }
-            for (int i = 0; i < nc; i++) {
-                c[i] = sc.nextInt();
-            }
-
-            List<Integer>[] adj = new ArrayList[na + nb + nc];
-
-            for (int i = 0; i < na + nb + nc; i++) {
-                adj[i] = new ArrayList<>();
+            for (int i =0;i<nc;i++) {
+                ac[i]=sc.nextInt();
             }
 
-            for (int i = 0; i < na; i++) {
-                for (int j = 0; j < nb; j++) {
-                    adj[i].add(na + j);
-                }
-            }
-
-            for (int i = 0; i < nb; i++) {
-                for (int j = 0; j < nc; j++) {
-                    adj[na + i].add(na + nb + j);
-                }
-            }
-
-
-            int min = Integer.MAX_VALUE;
-
-            for (int i = 0; i < na; i++) {
-                Deque<Integer> dq = new ArrayDeque<>();
-                dq.offer(i);
-
-                int[] abc = new int[3];
-
-                while (!dq.isEmpty()) {
-                    int cur = dq.poll();
-
-                    if (cur < na) {
-                        abc[0] = a[cur];
-                    } else if (cur < na + nb) {
-                        abc[1] = b[cur - na];
-                    } else {
-                        abc[2] = c[cur - na - nb];
-                    }
-
-                    if (adj[cur].isEmpty()) {
-                        min = Math.min(
-                                Math.max(
-                                        Math.max(
-                                                Math.abs(abc[0] - abc[1]),
-                                                Math.abs(abc[1] - abc[2])
-                                        ),
-                                        Math.abs(abc[0] - abc[2])
-                                ),
-                                min
-                        );
-                    }
-
-                    for (int neighbor : adj[cur]) {
-                        dq.offer(neighbor);
+            int min=Integer.MAX_VALUE;
+            for(int i : aa) {
+                for (int j : ab) {
+                    for (int k : ac) {
+                        min=Math.min(f(i, j, k), min);
                     }
                 }
             }
             System.out.println(min);
         }
+    }
+
+    private static int f(int i, int j, int k) {
+        return Math.max(
+                Math.max(
+                        Math.abs(i-j),
+                        Math.abs(i-k)
+                ),
+                Math.abs(j-k)
+        );
     }
 }
