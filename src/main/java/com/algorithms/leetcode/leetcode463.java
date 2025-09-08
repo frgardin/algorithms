ca7c7d1b02c7cab0c711c9629634faca8d44e76d
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-09-08 00:05:03 -0300

    format

--- src/main/java/com/algorithms/leetcode/leetcode463.java
+++ src/main/java/com/algorithms/leetcode/leetcode463.java
@@ -5,56 +5,54 @@ import java.util.Deque;
 
 //TODO failing
 public class leetcode463 {
-class Solution {
+    class Solution {
 
-    private static final int[][] dir = {{1, 0}, {-1, 0}, {0,1},{0,-1}};
+        private static final int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
 
-    public int islandPerimeter(int[][] grid) {
-        
-        int n=grid.length;
-        int m=grid[0].length;
+        public int islandPerimeter(int[][] grid) {
 
-        int[] start = getStart(grid, n, m);
+            int n = grid.length;
+            int m = grid[0].length;
 
-        int perimeter =0;
+            int[] start = getStart(grid, n, m);
 
-        Deque<int[]> dq = new ArrayDeque<>();
+            int perimeter = 0;
 
-        dq.offer(start);
+            Deque<int[]> dq = new ArrayDeque<>();
 
+            dq.offer(start);
 
-        while (!dq.isEmpty()) {
-            int[] cur = dq.poll();
+            while (!dq.isEmpty()) {
+                int[] cur = dq.poll();
 
-            for (int[] d : dir) {
-                int ni = cur[0]+d[0];
-                int nj = cur[1]+d[1];
+                for (int[] d : dir) {
+                    int ni = cur[0] + d[0];
+                    int nj = cur[1] + d[1];
 
-                if (ni >=0 && ni < n &&
-                    nj >=0 && nj < m                    
-                ) {
-                    if (grid[ni][nj] == 0) {
-                        perimeter++;
-                    } else {
-                        dq.offer(new int[]{ni, nj});
-                        grid[ni][nj]=0;
+                    if (ni >= 0 && ni < n &&
+                            nj >= 0 && nj < m) {
+                        if (grid[ni][nj] == 0) {
+                            perimeter++;
+                        } else {
+                            dq.offer(new int[] { ni, nj });
+                            grid[ni][nj] = 0;
+                        }
+                    }
+                }
+
+            }
+            return perimeter;
+        }
+
+        private static int[] getStart(int[][] grid, int n, int m) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (grid[i][j] == 1) {
+                        return new int[] { i, j };
                     }
                 }
             }
-
+            return new int[] { -1, -1 };
         }
-        return perimeter;
     }
-
-    private static int[] getStart(int[][] grid, int n, int m) {
-        for (int i=0;i<n;i++) {
-            for (int j=0;j<m;j++) {
-                if (grid[i][j] == 1){
-                    return new int[]{i, j};
-                }
-            }
-        }
-        return new int[]{-1, -1};
-    }
-}
 }
