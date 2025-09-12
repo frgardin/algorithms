71746309a314437c25ecbcefdfab1e824d0c1591
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-09-12 20:23:27 -0300

    c solution

--- src/main/java/com/algorithms/leetcode/leetcode58.java
+++ src/main/java/com/algorithms/leetcode/leetcode58.java
@@ -8,4 +8,23 @@ class leetcode58  {
         return ss[ss.length-1].length();
     }
 
+    public int lengthOfLastWord2(String s) {
+        int c = 0;
+        int i = s.length()-2;
+
+        if (s.charAt(i+1) != ' ') c++;
+
+
+        while (i >=0) {
+            if (s.charAt(i) == ' ' && s.charAt(i+1) != ' ') {
+                return c;
+            } 
+
+            if (s.charAt(i) != ' ') c++;
+
+            i--;
+        }
+        return c;
+    }
+
 }
