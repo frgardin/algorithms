9db8ceac1d58c6f14da9b5ba1a2697faa6ccafce
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-09-16 20:36:50 -0300

    Improves code formatting and clarifies integer parsing
    Enhances readability by standardizing indentation and spacing.
    Refines integer parsing logic for better maintainability and
    ensures consistent handling of negative numbers and overflow cases.

--- src/main/java/com/algorithms/leetcode/leetcode8.java
+++ src/main/java/com/algorithms/leetcode/leetcode8.java
@@ -3,34 +3,33 @@ package com.algorithms.leetcode;
 import java.util.Set;
 
 public class leetcode8 {
-        private static final Set<Character> NUMBERS = Set.of(
-                                                            '0', 
-                                                            '1', 
-                                                            '2',
-                                                            '3', 
-                                                            '4', 
-                                                            '5',
-                                                            '6', 
-                                                            '7', 
-                                                            '8',
-                                                            '9'
-                                                        );
+    private static final Set<Character> NUMBERS = Set.of(
+            '0',
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9');
 
     public int myAtoi(String s) {
-        int ans =0;
-        int i =0;
+        int ans = 0;
+        int i = 0;
 
         Character firstChar = null;
         boolean isNegative = false;
 
-        while (i<s.length()) {
+        while (i < s.length()) {
             if (firstChar == null) {
                 if (NUMBERS.contains(s.charAt(i))) {
                     firstChar = s.charAt(i);
-                    ans+=s.charAt(i)-'0';
-                } else if ( s.charAt(i) == '-') {
+                    ans += s.charAt(i) - '0';
+                } else if (s.charAt(i) == '-') {
                     firstChar = s.charAt(i);
-                    isNegative=true;
+                    isNegative = true;
                 } else if (s.charAt(i) == '+') {
                     firstChar = s.charAt(i);
                 } else if (s.charAt(i) == ' ') {
@@ -41,18 +40,18 @@ public class leetcode8 {
 
             } else {
                 if (!NUMBERS.contains(s.charAt(i))) {
-                   return isNegative ? (-1)*ans : ans;
+                    return isNegative ? (-1) * ans : ans;
                 }
-                int sum = s.charAt(i)-'0';
-                int possibleValue = ans*10+sum;
-                
-                if (possibleValue/10 != ans) {
+                int sum = s.charAt(i) - '0';
+                int possibleValue = ans * 10 + sum;
+
+                if (possibleValue / 10 != ans) {
                     return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
                 }
                 ans = possibleValue;
             }
             i++;
         }
-        return isNegative ? (-1)*ans : ans;
+        return isNegative ? (-1) * ans : ans;
     }
 }
