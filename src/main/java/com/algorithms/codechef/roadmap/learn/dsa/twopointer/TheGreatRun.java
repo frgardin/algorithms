02d4355009b070e57e8d5e366c0476467bb3b290
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-09-22 23:37:55 -0300

    problem

---
+++ src/main/java/com/algorithms/codechef/roadmap/learn/dsa/twopointer/TheGreatRun.java
@@ -0,0 +1,44 @@
+package com.algorithms.codechef.roadmap.learn.dsa.twopointer;
+
+import java.util.Scanner;
+
+public class TheGreatRun {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++)
+                a[i] = sc.nextInt();
+
+            int l = 0;
+            int r = k - 1;
+
+            int sum = 0;
+            for (int i = l; i <= r; i++) {
+                sum += a[i];
+            }
+
+            int ans = sum;
+
+            while (r < n - 1) {
+                ans = Math.max(ans, sum);
+                r++;
+                sum += a[r];
+                sum -= a[l];
+                l++;
+            }
+            ans = Math.max(ans, sum);
+
+            System.out.println(ans);
+        }
+        sc.close();
+    }
+}
