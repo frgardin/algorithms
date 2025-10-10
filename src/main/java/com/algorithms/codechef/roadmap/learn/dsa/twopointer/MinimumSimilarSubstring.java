20f910c24e7c110868d433c5d14e89a9e3455106
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-09 22:05:29 -0300

    trying to win

--- src/main/java/com/algorithms/codechef/roadmap/learn/dsa/twopointer/MinimumSimilarSubstring.java
+++ src/main/java/com/algorithms/codechef/roadmap/learn/dsa/twopointer/MinimumSimilarSubstring.java
@@ -5,46 +5,59 @@ import java.util.*;
 public class MinimumSimilarSubstring {
 
 	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		
-		sc.nextLine();
-		
-		String s = sc.nextLine();
-		String t = sc.nextLine();
-		
-		
-		int[] arrS = new int[26];
-		HashMap<Character, Integer> m1 = new HashMap<>(n);
-		HashMap<Character, Integer> m2 = new HashMap<>(m);
-		
-		
-		for (char c : s.toCharArray()) {
-		    m1.put(c, m1.getOrDefault(c,0)+1);
-		}
-		
-		for (char c : t.toCharArray()) {
-		    m2.put(c, m2.getOrDefault(c,0)+1);
-		}
-		
-		for (char c : t.toCharArray()) {
-		    if (m2.get(c) > m1.get(c)) {
-		        System.out.println("IMPOSSIBLE");
-		        return;
-		    } 
-		}
-		
-		int l=0;
-		int r=0;
-		
-		while (l<=r) {
-		    // TODO
-		}
-		
-		sc.close();
-	}
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        sc.nextLine();
+
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+
+        int[] freq = new int[26];
+
+        for (char c : t.toCharArray()) {
+            freq[c-'a']++;
+        }
+
+        int left = 0, count = 0, minLength = Integer.MAX_VALUE, start = -1;
+        int[] window = new int[26];
+
+
+        for (int right = 0; right < s.length(); right++) {
+            int index = s.charAt(right) - 'a';
+            window[index]++;
+
+            if (window[index] <= freq[index]) {
+                count++;
+            }
+
+            while (count == t.length()) {
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
+                    start = left;
+                }
+
+                index = s.charAt(left) - 'a';
+                window[index]--;
+
+                if (window[index] < freq[index]) {
+                    count--;
+                }
+
+                left++;
+            }
+        }
+
+        if (minLength == Integer.MAX_VALUE) {
+            System.out.println("IMPOSSIBLE");
+        } else {
+            System.out.println(minLength);
+            System.out.println(s.substring(start, start + minLength));
+        }
+
+    }
 }
