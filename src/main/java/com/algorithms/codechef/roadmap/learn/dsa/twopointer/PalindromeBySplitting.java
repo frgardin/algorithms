eaccd5aba196842f5b85baca4fc985645ce70189
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-12 11:19:44 -0300

    trying to win

--- src/main/java/com/algorithms/codechef/roadmap/learn/dsa/twopointer/PalindromeBySplitting.java
+++ src/main/java/com/algorithms/codechef/roadmap/learn/dsa/twopointer/PalindromeBySplitting.java
@@ -1,67 +1,32 @@
 package com.algorithms.codechef.roadmap.learn.dsa.twopointer;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
 
 public class PalindromeBySplitting {
 
-    //TODO: IS NOT WORKING
     public static void main(String[] args) {
         // your code goes here
         Scanner sc = new Scanner(System.in);
 
         int t = sc.nextInt();
 
-        while (t-->0) {
+        while (t-- > 0) {
             int n = sc.nextInt();
-
-            int[] a = new int[n];
-
-            for (int i=0;i<n;i++) a[i]=sc.nextInt();
 
             Deque<Integer> dq = new ArrayDeque<>();
 
-            int l, r, ans = 0;
+            for (int i = 0; i < n; i++) dq.addLast(sc.nextInt());
 
-            if (n % 2 == 0) {
-                l = n/2;
-                r = l+1;
-            } else {
-                l = n/2-1;
-                r = n/2+1;
-            }
+            int ans = 0;
 
-            if (!(l >=0 && r < n)) {
-                System.out.println(0);
-                continue;
-            }
-
-
-            dq.addFirst(a[l]);
-            dq.addLast(a[r]);
-
-            while (l >=0 && r < n) {
-                int leftValue = dq.pollFirst();
-                int rightValue = dq.pollLast();
-
-
-                if (leftValue == rightValue) {
-                    l--;
-                    r++;
-                    if (l >=0 && r < n) {
-                        dq.addFirst(a[l]);
-                        dq.addLast(a[r]);
-                    }
-                    continue;
-                }
-                if (leftValue < rightValue) {
-                    ans++;
-                    dq.addLast(leftValue);
-                    dq.addLast(rightValue - leftValue);
-                    continue;
-                }
+            while (dq.size() > 1) {
+                int l = dq.pollFirst(), r = dq.pollLast();
+                if (l == r) continue;
                 ans++;
-                dq.addFirst(rightValue);
-                dq.addFirst(leftValue - rightValue);
+                if (l < r) dq.addLast(r - l);
+                else dq.addFirst(l - r);
             }
 
             System.out.println(ans);
