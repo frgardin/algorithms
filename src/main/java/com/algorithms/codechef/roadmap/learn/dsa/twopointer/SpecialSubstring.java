24909140a48d5eba3de347c46d145a0bcd6e8176
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-13 21:40:41 -0300

    trying to win

--- src/main/java/com/algorithms/codechef/roadmap/learn/dsa/twopointer/SpecialSubstring.java
+++ src/main/java/com/algorithms/codechef/roadmap/learn/dsa/twopointer/SpecialSubstring.java
@@ -1,32 +1,31 @@
 package com.algorithms.codechef.roadmap.learn.dsa.twopointer;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class SpecialSubstring {
 
-    public static void main (String[] args) throws java.lang.Exception
-    {
+    public static void main(String[] args) throws java.lang.Exception {
         // your code goes here
         Scanner sc = new Scanner(System.in);
 
-        int t=sc.nextInt();
+        int t = sc.nextInt();
 
-        while (t-->0) {
-            int n=sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
             sc.nextLine();
             String s = sc.nextLine();
 
             int[] a = new int[26];
 
-            int l=0, r=0, max=0;
+            int l = 0, r = 0, max = 0;
 
-            while (r<n) {
-                while (a[s.charAt(r)-'a'] > s.charAt(r)-'a') {
-                    a[s.charAt(l)-'a']--;
+            while (r < n) {
+                while (a[s.charAt(r) - 'a'] > s.charAt(r) - 'a') {
+                    a[s.charAt(l) - 'a']--;
                     l++;
                 }
-                max=Math.max(max, r-l+1);
-                a[s.charAt(r)-'a']++;
+                max = Math.max(max, r - l + 1);
+                a[s.charAt(r) - 'a']++;
                 r++;
             }
 
