1407b6fbf5e77b3b8e3df4af978a338e0e012e81
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-13 21:40:36 -0300

    trying to win

---
+++ src/main/java/com/algorithms/codechef/roadmap/learn/dsa/twopointer/SpecialSubstring.java
@@ -0,0 +1,36 @@
+package com.algorithms.codechef.roadmap.learn.dsa.twopointer;
+
+import java.util.*;
+
+public class SpecialSubstring {
+
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+
+        int t=sc.nextInt();
+
+        while (t-->0) {
+            int n=sc.nextInt();
+            sc.nextLine();
+            String s = sc.nextLine();
+
+            int[] a = new int[26];
+
+            int l=0, r=0, max=0;
+
+            while (r<n) {
+                while (a[s.charAt(r)-'a'] > s.charAt(r)-'a') {
+                    a[s.charAt(l)-'a']--;
+                    l++;
+                }
+                max=Math.max(max, r-l+1);
+                a[s.charAt(r)-'a']++;
+                r++;
+            }
+
+            System.out.println(max);
+        }
+    }
+}
