7a75b66cc2f1dc299943f5a444e1746e7d3fd543
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-15 17:46:26 +0000

    solved

--- src/main/cpp/codechef/prefixsum/mystical_numbers.cpp
+++ src/main/cpp/codechef/prefixsum/mystical_numbers.cpp
@@ -1,42 +1,48 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main() 
+typedef long long int ll;
+
+int main()
 {
-	// your code goes here
-    int t;
-    
-    cin >> t;
-    
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        vector<int> v(n);
-        
-        for (int i=0;i<n;i++) cin >> v[i];
-        
-        int q;
-        cin >> q;
-        
-        while (q--) 
-        {
-            int l, r, x;
-            cin >> l >> r >> x;
-            
-            int ans=0;
-            
-            for (int i =l-1;i<r;i++) {
-                int op1=v[i]^x;
-                int op2=v[i]&x;
-                if (op1 > op2) {
-                    ans++;
-                }
-            }
-            
-            cout << ans << endl;
+    int t; cin >> t;
+
+    auto get_msb = [] (ll x) {
+        int max =31;
+        while (max>=0) {
+            if (x & (1LL << max)) return max;
+            max--;
+        }
+        return 32;
+    };
+
+    while (t--) {
+        int n; cin >> n;
+        vector<ll> v(n);
+
+        for (ll& x : v) cin >> x;
+
+        vector<array<ll, 33>> pre(n);
+
+        for (int i =0;i<n;i++) {
+            if (i) pre[i]=pre[i-1];
+            pre[i][get_msb(v[i])]++;
+        }
+
+        int q;cin >> q;
+
+        auto get = [&] (int l, int r, int b) {
+            int ret = pre[r][b];
+            if (l) ret-=pre[l-1][b];
+            return ret;
+        };
+
+        while (q--) {
+            int l, r,x;
+            cin >> l >> r >>x;
+
+            cout << r-l+1 - get(l-1, r-1, get_msb(x)) << endl;
         }
     }
-
 }
-
