6d3641854cd260856b9946a4f50680883f2f6fc6
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-16 11:24:34 -0300

    formatting

--- src/main/cpp/codechef/bynary_search/search_insert_position.cpp
+++ src/main/cpp/codechef/bynary_search/search_insert_position.cpp
@@ -2,24 +2,33 @@
 
 using namespace std;
 
-int search_insert_position(vector<int> v, int n, int k) {
-    int l=0,r=n-1;
-    while (l<=r) {
-        int m = (l+r)/2;
-        if (v[m] == k) return m;
-        if (v[m] > k) r =m-1;
-        else          l=m+1;
+int search_insert_position(vector<int> v, int n, int k)
+{
+    int l = 0, r = n - 1;
+    while (l <= r)
+    {
+        int m = (l + r) / 2;
+        if (v[m] == k)
+            return m;
+        if (v[m] > k)
+            r = m - 1;
+        else
+            l = m + 1;
     }
     return l;
 }
 
-int main() {
+int main()
+{
     int n, q;
     cin >> n >> q;
     vector<int> v(n);
-    for (int &x : v) cin >> x;
-    while (q--) {
-        int k; cin >> k;
+    for (int &x : v)
+        cin >> x;
+    while (q--)
+    {
+        int k;
+        cin >> k;
         cout << search_insert_position(v, n, k) << endl;
     }
 }
