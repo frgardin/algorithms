bdab31284d85fa1b113d3f97665eda4a8d87fe2f
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-19 09:01:14 -0300

    my code now

--- src/main/cpp/codechef/bynary_search/aggressive_cows.cpp
+++ src/main/cpp/codechef/bynary_search/aggressive_cows.cpp
@@ -3,48 +3,49 @@ using namespace std;
 
 bool check(int arr[], int d, int n, int c)
 {
-    int prev = arr[0];c--;
-    for(int i=1;i<n;i++) {
-        if (arr[i]-prev >= d) {
+    int prev = arr[0];
+    c--;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] - prev >= d)
+        {
             c--;
-            prev=arr[i];
+            prev = arr[i];
         }
-        if (c==0) return true;
+        if (c == 0)
+            return true;
     }
     return false;
 }
 
-void test_case(){
+void test_case()
+{
     int n, c;
-    cin>>n>>c;
+    cin >> n >> c;
     int arr[n];
     int maxnum = INT_MIN;
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
-        maxnum = max(maxnum, arr[i]);
+        cin >> arr[i];
     }
-    sort(arr, arr+n);
-    int start = 0, end = maxnum;
-    while(abs(end-start)>1)
-    {
-        int median = (start+end)/2;
-        if(check(arr, median, n, c))
-        {
-            start = median;
-        }
-        else 
-        {
-            end = median;
-        }
-    }
-    cout<<start<<"\n";
+    sort(arr, arr + n);
+    // Write your code here
+    int maxnum = 0;
+    for (int x : arr)
+        maxnum = max(x, maxnum);
+
+    int start, end;
+    for (start = 0, end = maxnum;
+         abs(start - end) > 1;
+         (check(arr, (start + end) / 2, n, c) ? start = (start + end) / 2 : end = (start + end) / 2)
+    );
+
+    cout << start << endl;
 }
- 
- 
- 
+
 int main()
 {
     int T = 1;
-    while(T--) test_case();
+    while (T--)
+        test_case();
 }
