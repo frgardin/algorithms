e57bf6017660bbae3abec835eca25230324af149
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-19 11:49:39 -0300

    formatting

--- src/main/cpp/codechef/bynary_search/ball_position.cpp
+++ src/main/cpp/codechef/bynary_search/ball_position.cpp
@@ -1,33 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     // Write your code here
     int n, k;
     cin >> n >> k;
-    vector<int>v(n);
-    for (int &x : v) cin >> x;
-    int maxnum=0;
-    for (int x : v) maxnum=max(maxnum, x);
+    vector<int> v(n);
+    for (int &x : v)
+        cin >> x;
+    int maxnum = 0;
+    for (int x : v)
+        maxnum = max(maxnum, x);
     sort(v.begin(), v.end());
-    auto check = [] (vector<int>v, int n, int d, int k) {
-        int prev=v[0];k--;
-        for (int i=1;i<n;i++) {
-            if (v[i]-prev>=d) {
+    auto check = [](vector<int> v, int n, int d, int k)
+    {
+        int prev = v[0];
+        k--;
+        for (int i = 1; i < n; i++)
+        {
+            if (v[i] - prev >= d)
+            {
                 k--;
-                prev=v[i];
+                prev = v[i];
             }
-            if (k==0) return true;
+            if (k == 0)
+                return true;
         }
         return false;
     };
-    int start=0,end=maxnum;
-    while (abs(start-end)>1) {
-        int median=(start+end)/2;
-        if (check(v, n, median, k)) {
-            start=median;
-        } else {
-            end=median;
+    int start = 0, end = maxnum;
+    while (abs(start - end) > 1)
+    {
+        int median = (start + end) / 2;
+        if (check(v, n, median, k))
+        {
+            start = median;
+        }
+        else
+        {
+            end = median;
         }
     }
     cout << start << endl;
