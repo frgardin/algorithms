87a025ca8e0fd26847e42b5975aeb4be9df297c5
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-19 02:45:51 +0000

    formatting

--- src/main/cpp/codechef/bynary_search/bynary_search_demo.cpp
+++ src/main/cpp/codechef/bynary_search/bynary_search_demo.cpp
@@ -3,25 +3,31 @@
 using namespace std;
 typedef long long ll;
 
-ll bynary_search(vector<ll> v, int n, ll k) {
-    int l=0,r=n-1;
-    while (l<=r) {
-        int m=(l+r)/2;
-        if (v[m] == k)  return m;
-        if (v[m] > k)   r=m-1;
-        else            l=m+1;
+ll bynary_search(vector<ll> v, int n, ll k)
+{
+    int l = 0, r = n - 1;
+    while (l <= r)
+    {
+        int m = (l + r) / 2;
+        if (v[m] == k)
+            return m;
+        if (v[m] > k)
+            r = m - 1;
+        else
+            l = m + 1;
     }
 
     return -1;
 }
 
-
-int main() {
+int main()
+{
     int n;
     ll k;
     cin >> n >> k;
-    vector<ll> v (n);
-    for (ll &x : v) cin >> x;
+    vector<ll> v(n);
+    for (ll &x : v)
+        cin >> x;
     cout << bynary_search(v, n, k) << endl;
     return 0;
 }
