b276ddf55d240c2335e9f30016c26d8c4e07c413
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-20 17:54:50 -0300

    formatting

--- src/main/cpp/codechef/recursion/linear_search.cpp
+++ src/main/cpp/codechef/recursion/linear_search.cpp
@@ -1,18 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int linearSearch(vector<int>& arr, int n, int x) {
-    //Write your code here
-    if (n==0) return -1;
-    if (arr[n-1] == x) return n-1;
-    return linearSearch(arr, n-1, x);
+int linearSearch(vector<int> &arr, int n, int x)
+{
+    // Write your code here
+    if (n == 0)
+        return -1;
+    if (arr[n - 1] == x)
+        return n - 1;
+    return linearSearch(arr, n - 1, x);
 }
 
-int main() {
+int main()
+{
     int n, x;
     cin >> n >> x;
     vector<int> arr(n);
-    for (int i = 0; i < n; i++) cin >> arr[i];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
     cout << linearSearch(arr, n, x) << endl;
     return 0;
 }
