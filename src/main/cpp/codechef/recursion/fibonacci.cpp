988b98e4b59986865097dddfec612773369e066c
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-20 15:06:46 -0300

    msg

---
+++ src/main/cpp/codechef/recursion/fibonacci.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n) {
+    //Write your code here
+    if (n==1) return 0;
+    if (n==2 || n == 3) return 1;
+    return fib(n-1)+fib(n-2);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fib(n) << endl;
+    return 0;
+}
