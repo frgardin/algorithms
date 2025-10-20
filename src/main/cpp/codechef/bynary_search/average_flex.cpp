da6fc779cf88e5f1172a68df31d44040702654d4
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-20 13:54:46 -0300

    playing with typedef

--- src/main/cpp/codechef/bynary_search/average_flex.cpp
+++ src/main/cpp/codechef/bynary_search/average_flex.cpp
@@ -1,20 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef vector<int> vi;
+typedef int i;
+
 int main() {
 	// your code goes here
-	int t; cin >> t;
+	i t; cin >> t;
 	while(t--)
 	{
-	    int n; cin >> n;
-	    vector<int> v(n);
+	    i n; cin >> n;
+	    vi v(n);
 	    for(int &x : v) cin >> x;
 	    sort(v.begin(), v.end());
 	    int count=0;
-	    for (int &x : v) 
+	    for (i &x : v) 
 	    {
-	        int less_equal = upper_bound(v.begin(), v.end(), x) - v.begin();
-	        int greater = n - less_equal;
+	        i less_equal = upper_bound(v.begin(), v.end(), x) - v.begin();
+	        i greater = n - less_equal;
 	        if (less_equal>greater) count++;
 	    }
 	    cout << count << endl;
