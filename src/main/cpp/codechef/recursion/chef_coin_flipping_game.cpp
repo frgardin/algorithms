ea668fbdfcb39e2443e8d73514f78d9442bf8849
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-22 12:09:55 -0300

    one more

---
+++ src/main/cpp/codechef/recursion/chef_coin_flipping_game.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<vector<int>> vii;
+typedef vector<int> vi;
+
+void backtrack(int s, int i, int &cur, int &count) {
+    if (cur == s) {
+        count++;
+        return;
+    }
+    if (cur >s) return;
+    for (int j=1;j<=2;j++) {
+        cur+=j;
+        backtrack(s,j, cur, count);
+        cur-=j;
+    }
+}
+
+int countWays(int s) {
+    // Write your code here
+    int ans=0;
+    int cur=0;
+    backtrack(s, 0, cur, ans);
+    return ans;
+}
+
+int main() {
+    int S;
+    cin >> S;
+    cout << countWays(S) << endl;
+    return 0;
+}
