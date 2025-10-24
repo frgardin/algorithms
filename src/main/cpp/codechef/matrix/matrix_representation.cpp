099871cfac59c105023113c1ad0f524949332679
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2025-10-24 08:51:50 -0300

    formatting

--- src/main/cpp/codechef/matrix/matrix_representation.cpp
+++ src/main/cpp/codechef/matrix/matrix_representation.cpp
@@ -1,26 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int n;
-  cin >> n;
+int main()
+{
+    int n;
+    cin >> n;
 
-  assert(1 <= n && n <= 100);
+    assert(1 <= n && n <= 100);
 
-  vector<vector<int> > mat(n, vector<int> (n));
+    vector<vector<int>> mat(n, vector<int>(n));
 
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < n; j++){
-      // complete the code
-      mat[i][j] = i*n+j+1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            // complete the code
+            mat[i][j] = i * n + j + 1;
+        }
     }
-  } 
-  
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < n; j++){
-       cout << mat[i][j] << ' ';
-    }
-  } 
 
-  return 0;
-} 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << mat[i][j] << ' ';
+        }
+    }
+
+    return 0;
+}
